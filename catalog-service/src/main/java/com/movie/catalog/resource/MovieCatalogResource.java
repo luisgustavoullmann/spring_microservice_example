0c11187ad27c33363ba19234ea211b57967beca0
luisgustavoullmann <luisgustavoullmann@gmail.com>
2020-05-12 16:15:22 -0300

    WebClient(for reactive programming) instead of RestTemplate

--- catalog-service/src/main/java/com/movie/catalog/resource/MovieCatalogResource.java
+++ catalog-service/src/main/java/com/movie/catalog/resource/MovieCatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,25 +18,32 @@ import com.movie.catalog.model.Rating;
 @RestController //API
 @RequestMapping("/catalog") //URL
 public class MovieCatalogResource {	
+	
+	//Creating a instance for the RestTemplate make this calls
+	//Using Bean to create a single instance
+	@Autowired //Dependency injection, fetch bean annotation
+	//RestTemplate will be deprecated, use the alternative WebClient
+	private RestTemplate restTemplate; //the name doesn't matter, the type matters
+	
+	
+	//@Autowired
+	//private WebClient.Builder webClientBuilder;//Dependency injection for reactive program
 
-	@GetMapping("/{userId}") //Passando userId como variável
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+	@GetMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){		
 		
-		//Creating a instante for the RestTemplate make this calls
-		RestTemplate restTemplate = new RestTemplate();
-		//Function to make a rest call, parameters (URL and class)
-		//gets back a string, so you provide a class with the same properties
-		//as the JSON, RestTemplate is going to create the instance of the class
-		//populate those forms for you and give you a fully object
-		Movie movie = restTemplate.getForObject("http://localhost:8082/movies/foo", Movie.class);			
-		
+		//Using WebClient as a alternative for the RestTemplate
+		//WebClient doesn't exist in the class path, 
+		//is based on reacting programming space or asynchronous programming
+		//Spring Initializr - In pom.xml, import dependecy reactive web with Spring Webflux
+		//WebClient.Builder builder = WebClient.builder(); create bean for this	
 		
 		//The communication happens through Rest template,
 		//what you get back is a string,
 		//but you need objects (for a particularly class) to detail with the data
 		//and that's why we have to copy the models from APIs
 		List<Rating> ratings = Arrays.asList(
-				//harcode the response we got from data API
+				//hard code the response we got from data API
 				new Rating("1234", 4), //movieId and rate
 				new Rating("6789", 3)
 		);
@@ -44,7 +52,26 @@ public class MovieCatalogResource {
 		//stream, group of data
 		// map then and 
 		return ratings.stream().map(rating -> {
-			//new CatalogItem("Spider-Man", "About a guy", 4);
+			//hard coded new CatalogItem("Spider-Man", "About a guy", 4);
+			
+			//Function to make a rest call, parameters (URL and class)
+			//gets back a string, so you provide a class with the same properties
+			//as the JSON, RestTemplate is going to create the instance of the class
+			//populate those forms for you and give you a fully object
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			
+			//Asynchronous - Reacting programmimg - Webflux
+			/*Instance in reactive programming, the line above will be replaced by this:
+			Movie movie = webClientBuilder.build() //creating new instance
+				.get() //calling the method (Get or Post..)
+				.uri("http://localhost:8082/movies/" + rating.getMovieId())
+				.retrieve() //Fetch the data, giving you what the method is, so go do it.
+				//bodyToMono - Something like a promise (JS), will get to you what you want in a future time
+				//creating an asynchronous object. Or is like you're holding an empty container to use it in a future moment.
+				.bodyToMono(Movie.class) //elementClass - Get whatever you calling and give me the instance of Movie.class
+				.block(); //block when you already have the execution/object
+			*/
+			
 			return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
 		
 		})
--- catalog-service/src/main/java/com/movie/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/movie/catalogservice/CatalogServiceApplication.java
@@ -2,6 +2,9 @@ package com.movie.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CatalogServiceApplication {
@@ -9,6 +12,18 @@ public class CatalogServiceApplication {
 	//CORE APP
 	
 	//You need the APIs model, to store the infos
+	
+	@Bean //Bean is a producer, so save this instance somewhere
+	//RestTemplate will be deprecated, use the alternative WebClient
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+	//Just create this Bean with in your projet you are using reactive programmimg
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
